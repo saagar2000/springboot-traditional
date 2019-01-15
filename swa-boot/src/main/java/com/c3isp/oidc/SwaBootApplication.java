535bf1e44f72ab98911f4b37aaa207d65aabe037
ewd5 <ewd5@ewd5-lt.ad.kent.ac.uk>
2019-01-15 11:16:54 +0000

    test

---
+++ swa-boot/src/main/java/com/c3isp/oidc/SwaBootApplication.java
@@ -0,0 +1,14 @@
+package com.c3isp.oidc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaBootApplication.class, args);
+	}
+
+}
+
